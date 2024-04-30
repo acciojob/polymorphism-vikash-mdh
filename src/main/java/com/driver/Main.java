a515b6769220c32e7e09918a9f2b3d8e27e56a4c
vikash-mdh <vikashkumar555550@gmail.com>
2024-04-30 23:57:06 +0530

    fix in error

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,10 +3,10 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         Product p = new Product();
-        int ans = p.Product(10, 20);
-        int ans1 = p.Product(10,20,30);
+       // int ans = p.Product(10, 20);
+        //int ans1 = p.Product(10,20,30);
         double ans3 = p.product(1.0,2.0);
-        System.out.println(ans+" "+ ans1+" "+ans3);
+        System.out.println(ans3);
 
     }
     public static class Product{
