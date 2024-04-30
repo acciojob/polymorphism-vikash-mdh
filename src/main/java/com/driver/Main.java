874349e40b2e1f4dff6769b9a93f6e8660283af9
vikash-mdh <vikashkumar555550@gmail.com>
2024-05-01 00:08:02 +0530

    fixit error

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,29 +1,29 @@
 package com.driver;
 
 public class Main {
-    public static class Product{
-        public int Product(int x, int y) {
-            return x*y;
-        }
-        public int Product(int x, int y, int z) {
-            return x*y*z;
-        }
-        public double product(double x, double y) {
-            return x*y;
-        }
-    }
+    public static class Product {
 
+        public int product(int x, int y) {
+            return x * y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        public double product(double x, double y) {
+            return x * y;
+        }
+
+    }
 
     public static void main(String[] args) {
         Product p = new Product();
-       double ans = p.product(15.1, 15.2);
-       int ans1 = p.Product(10,20);
-       int ans3 = p.Product(10,20,30);
-        System.out.println(ans);
-        System.out.println(ans1);
-        System.out.println(ans3);
-
+        double res = p.product(15.5, 15.2);
+        int sum = p.product(12, 5);
+        int sub = p.product(15, 12);
+        System.out.println(sum);
+        System.out.println(res);
+        System.out.println(sub);
     }
-
-
 }
