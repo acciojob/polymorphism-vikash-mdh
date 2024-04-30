fdce0c419f628d13513acb5d526aff9fdb348656
vikash-mdh <vikashkumar555550@gmail.com>
2024-04-30 23:28:25 +0530

    add some faature

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p = new Product();
+        int ans = p.Product(10, 20);
+        int ans1 = p.Product(10,20,30);
+        double ans3 = p.product(1.0,2.0);
+        System.out.println(ans+" "+ ans1+" "+ans3);
+
+    }
+   static class Product{
+        public int Product(int x, int y) {
+            return x*y;
+        }
+       public int Product(int x, int y, int z) {
+            return x*y*z;
+       }
+       public double product(double x, double y) {
+            return x*y;
+       }
+    }
 
 }
