ddaf3925dcc2aeec53eaac1e034a3589f6c23062
vikash-mdh <vikashkumar555550@gmail.com>
2024-05-01 00:04:28 +0530

    fix in error

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,24 +1,29 @@
 package com.driver;
 
 public class Main {
-    public static void main(String[] args) {
-        Product p = new Product();
-       // int ans = p.Product(10, 20);
-        //int ans1 = p.Product(10,20,30);
-        double ans3 = p.product(1.0,2.0);
-        System.out.println(ans3);
-
-    }
     public static class Product{
         public int Product(int x, int y) {
             return x*y;
         }
-       public int Product(int x, int y, int z) {
+        public int Product(int x, int y, int z) {
             return x*y*z;
-       }
-       public double product(double x, double y) {
+        }
+        public double product(double x, double y) {
             return x*y;
-       }
+        }
     }
+
+
+    public static void main(String[] args) {
+        Product p = new Product();
+       double ans = p.product(15.1, 15.2);
+       int ans1 = p.Product(10,20);
+       int ans3 = p.Product(10,20,30);
+        System.out.println(ans);
+        System.out.println(ans1);
+        System.out.println(ans3);
+
+    }
+
 
 }
