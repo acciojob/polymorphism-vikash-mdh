022b060d9f51f262e20f88e45214cd7eb5b0aafd
vikash-mdh <vikashkumar555550@gmail.com>
2024-04-30 23:50:58 +0530

    fix in error

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -9,7 +9,7 @@ public class Main {
         System.out.println(ans+" "+ ans1+" "+ans3);
 
     }
-   static class Product{
+    public static class Product{
         public int Product(int x, int y) {
             return x*y;
         }
